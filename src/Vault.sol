508e3c99878500d3a0dcde5c93c4e067b27fffba
Vaibhav <vaibhavritam@gmail.com>
2026-01-16 13:49:43 +0800

    Initial init

---
+++ src/Vault.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+
+contract Vault {
+
+    mapping(address client => uint256 balance) sBalances;
+    address private immutable iOwner;
+
+    constructor() {
+        iOwner = msg.sender;
+    }
+
+    function deposit(uint256 _secondsToLockMoney) payable external{
+        
+    }
+}
